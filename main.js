826b6ff93940eff4d9bb8c8d9defe58c3d9b8a9c
Rakhimova Shirin <examle@mail.com>
2024-01-08 17:20:55 +0500

    add function with timer

--- main.js
+++ main.js
@@ -9,3 +9,10 @@ function summ(a = 0, b = 0){
 function diff (a, b){
     return a - b
 }
+
+
+function sayHi(){
+    return new Promise(res => {
+        setTimeout(() => res('hi'), 1000)
+    })
+}
